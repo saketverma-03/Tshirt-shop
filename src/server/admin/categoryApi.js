1de68f196286cb6cb4dbdf13c60269b0d14eb90e
saket verma <saketv831@gmail.com>
2021-04-16 23:57:35 +0530

    added alll the apis for category request

--- src/server/admin/categoryApi.js
+++ src/server/admin/categoryApi.js
@@ -20,3 +20,30 @@ export const getCategorys = () =>
   fetch(`${API}/categorys`)
     .then((res) => res.json())
     .catch((e) => console.log(e));
+
+// Update Category
+export const updateCategory = (categoryId, userId, token, name) =>
+  fetch(`${API}/category/updateCategory/${categoryId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: { name: name },
+  })
+    .then((res) => res.json())
+    .catch((e) => console.error(e));
+
+// Delete Category
+export const deleteCategory = (categoryId, userId, token) =>
+  fetch(`${API}/category/updateCategory/${categoryId}/${userId}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .catch((e) => console.error(e));
