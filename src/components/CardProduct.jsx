e3d4ff24478164fc47448f1c65d56a3f6952d8d8
saket verma <saketv831@gmail.com>
2021-04-22 17:07:04 +0530

    added hover effect to product card

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -5,7 +5,7 @@ const CardProduct = ({ name, price, photo, id }) => {
     //
   };
   return (
-    <div className="rounded-ms flex border-indigo-10 border-2 shadow-sm   flex-col h-96 w-full max-w-sm md:mr-3 md:h-72 md:w-72 lg:h-80 lg:w-72 mb-3 bg-white">
+    <div className="transition-colors ease-in-out rounded-ms flex border-indigo-10 border-2 shadow-sm flex-col h-96 w-full max-w-sm md:mr-3 md:h-72 md:w-72 lg:h-80 lg:w-72 mb-3 bg-white  hover:border-indigo-400">
       <div className="h-4/6 w-full">
         <img className="rounded-sm h-full w-full" src={photo} alt="A image" />
       </div>
