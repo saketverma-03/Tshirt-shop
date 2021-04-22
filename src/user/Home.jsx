a687190480f3882712b38e7cf540f263fa32b5d6
saket verma <saketv831@gmail.com>
2021-04-22 16:52:06 +0530

    Added filter by category functionality

--- src/user/Home.jsx
+++ src/user/Home.jsx
@@ -1,30 +1,47 @@
 import React, { useEffect, useState } from "react";
 import { CardProduct } from "../components";
 import { headerContext } from "../context";
-import { getAllProducts } from "../server/admin/productApi";
+import { getAllProducts, getCategorys } from "../server";
 
 function Home(props) {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  // TODO: optemize code to make reusable functions
+  // VARIABLES
+
+  // List of All ther products (to be renderd in card)
   const [productList, setProductList] = useState([]);
-  // setProductList([]);
-  console.log("Typeis", typeof productList);
-  // const arr = [1];
+
+  // List of All categorys
+  const [categorys, setCategorys] = useState([]);
+
+  // list of shorted Products (By category)
+  const [shortedList, SetShortedList] = useState([]);
+
+  // Handling Header Visibility
   const { setHeadHide } = React.useContext(headerContext);
   setHeadHide(false);
 
-  // Set Functions
-  const setProducts = () => {
-    getAllProducts()
-      .then((data) => {
-        console.log(data);
-        if (!data.error) {
-          console.log("All PRODUCTS FOUND SUCESS");
-          return setProductList([...data]);
-        }
-        console.log(data);
-        return console.log("ERROR::", data.error);
-      })
-      .catch((e) => console.log(e));
+  // SET FUNCTIONS
+
+  /* This Method Sets the ProductList  which is renderd as Card
+    -> if a shorderd list is given then it sets it as list 
+    -> esle sets list parapeter as the List of Products
+    (mostly a shorted lis is passed to get shoting functonality)
+  */
+  const setProducts = (list) => {
+    if (!list || list.length === 0) {
+      return getAllProducts()
+        .then((data) => {
+          console.log(data);
+          if (!data.error) {
+            console.log("All PRODUCTS FOUND SUCESS");
+            console.log(data);
+            return setProductList([...data]);
+          }
+          console.log(data);
+          return console.log("ERROR::", data.error);
+        })
+        .catch((e) => console.log(e));
+    } else return setProductList([...list]);
   };
 
   useEffect(() => {
@@ -40,26 +57,64 @@ function Home(props) {
       })
       .catch((e) => console.log(e));
   }, []);
-  // HandleresFunctions
+  useEffect(() => {
+    setAllCategorys();
+  }, []);
+
+  const setAllCategorys = () =>
+    getCategorys()
+      .then((res) =>
+        !res.error
+          ? // ? console.log(res)
+            setCategorys([...res])
+          : console.error(":: => Cant load categorys")
+      )
+      .catch((e) => console.log(e));
+
+  // returns a shorted list (By category)
+  const shortByCategory = (id) => {
+    if (id === "") return setProducts();
+    getAllProducts()
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          return [];
+        } else {
+          let temp = data.filter((item) => item.category === id);
+          setProducts([...temp]);
+        }
+      })
+      .catch((e) => console.log(e));
+  };
+
+  // HANDLER FUNCTIONS
   return (
     <div className=" flex-1 h-full grid grid-cols-1 md:grid-cols-12 md:grid-rows-none">
       <div className="h-full md:col-span-3 md:row-span-full bg-indigo-50 ">
         <h1 className="text-sm text-gray-600">Filters</h1>
 
         <ol className="flex flex-col ml-10 mt-10 flex-1 space-y-3 text-black cursor-pointer  font-sans text-sm ">
-          <h1 className="text-lg text-gray-600">Category</h1>
-          <li className="ml-5 hover:text-blue-600">Summer</li>
-          <li className="ml-5 hover:text-blue-600">Winter</li>
-          <li className="ml-5 hover:text-blue-600">Code Man</li>
-          <li className="ml-5 hover:text-blue-600">Leet code</li>
-          <li className="ml-5 hover:text-blue-600">LCO</li>
+          <h1 className="text-lg text-gray-600">
+            <button onClick={() => shortByCategory("")}>
+              {"All products"}
+            </button>
+          </h1>
+          {categorys.map((item) => {
+            return (
+              <h1 key={item._id} className="text-lg text-gray-600">
+                <button onClick={() => shortByCategory(item._id)}>
+                  {item.name}
+                </button>
+              </h1>
+            );
+          })}
         </ol>
       </div>
       <div className="overflow-y-auto  w-full h-screen row-span-full col-span-9 flex flex-row flex-wrap p-2 pt-4 pb-10 justify-center items-center md:justify-evenly bg-gray-50">
         {productList.map((product, i) => {
           return (
             <CardProduct
-              key={i}
+              key={product._id}
               name={product.name}
               price={product.price}
               id={product._id}
