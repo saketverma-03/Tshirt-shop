e81eb9615e3a02f75f1e222f936fd5b39a6490b5
saket verma <saketv831@gmail.com>
2021-04-22 11:34:25 +0530

    siplified

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,14 +1,20 @@
 import React, { useContext } from "react";
 import { headerContext } from "../context";
 import Menu from "./Menu";
+import { Loading } from ".";
 
 const Base = ({ children }) => {
   const { headhide } = useContext(headerContext);
   return (
     <>
+    <div className="overflow-hidden h-screen w-screen" >
       {!headhide && <Menu />}
-      <div className="flex h-screen justify-center items-center  bg-gray-100">
+      <div className="flex h-screen w-screen bg-indigo-100">
+        {/* <div className="flex-1 bg-gray-600 " > */}
+        {/* <Loading /> */}
         {children}
+        {/* </div> */}
+      </div>
       </div>
     </>
   );
